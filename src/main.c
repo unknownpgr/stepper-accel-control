81ea3c6ff336e37e37fc371abaa2da3201206649
unknownpgr <unknownpgr@gmail.com>
2024-09-25 12:51:36 +0000

    Add homing stage

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "pico/multicore.h"
 #include "hardware/timer.h"
 #include "hardware/gpio.h"
+#include "math.h"
 
 #define abs(x) ((x) < 0 ? -(x) : (x))
 
@@ -13,8 +14,9 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-volatile uint32_t position = 0; // Steps
-volatile float velocity = 0;    // Revolutions per second
+volatile float acceleration = 0.0f; // Revolutions per second squared
+volatile float velocity = 0;        // Revolutions per second
+volatile uint32_t position = 0;     // Steps
 
 /**
  * Core 0 is responsible for acceleration control
@@ -36,28 +38,67 @@ void core0_main()
     printf("UART initialized\n");
 
     // Acceleration control variables
-    float acceleration = 0.0f;
-    uint32_t previous_time = time_us_32();
     int32_t sign = 1;
     int32_t acceleration_buffer = 0;
 
     // Log variables
     uint32_t last_log_time = time_us_32();
+    uint32_t log_interval = 10000; // 10ms
 
     // Stop switch variables
     const uint STOP_SWITCH_PIN = 16;
     gpio_init(STOP_SWITCH_PIN);
     gpio_set_dir(STOP_SWITCH_PIN, GPIO_IN);
 
+    // Initialization step
+    {
+        // Fast homing
+        printf("Initializing...\n");
+        velocity = 3;
+        while (!gpio_get(STOP_SWITCH_PIN))
+            ;
+        velocity = 0;
+
+        // Slightly back off from the switch
+        position = 100;
+        velocity = -3;
+        while (position > 0)
+            ;
+
+        // Precise homing
+        velocity = 0.25;
+        while (!gpio_get(STOP_SWITCH_PIN))
+            ;
+        position = 1900; // Full distance is 3800 steps
+        velocity = 0;
+
+        float a = 90; // Maximum acceleration available
+        float min_speed_const = -999990;
+        while (position > 0)
+        {
+            float min_speed_dist = -sqrt(2 * a * position / 400.f);
+            if (velocity <= min_speed_dist)
+            {
+                velocity = min_speed_dist;
+            }
+            else if (velocity <= min_speed_const)
+            {
+                velocity = min_speed_const;
+            }
+            else
+            {
+                acceleration = -a;
+            }
+        }
+        velocity = 0;
+        acceleration = 0;
+    }
 
     // Main loop
     while (true)
     {
         // Update velocity
         uint32_t current_time = time_us_32();
-        uint32_t delta_time = current_time - previous_time;
-        previous_time = current_time;
-        velocity += acceleration * delta_time / 1000000.0f;
 
         // Read acceleration from USB serial
         int c = getchar_timeout_us(0);
@@ -81,6 +122,9 @@ void core0_main()
         if (current_time - last_log_time >= log_interval)
         {
             last_log_time += log_interval;
+            // printf("P:%luV:%f\n", position, velocity);
+        }
+
         // If the stop switch is pressed, set both velocity and acceleration to 0
         if (gpio_get(STOP_SWITCH_PIN))
         {
@@ -123,14 +167,18 @@ void core1_main()
         {1, 0, 0, 1},
     };
 
-    uint64_t max_step_time_us = 100000; // 100ms
+    // Step motor variables
+    uint64_t max_interval = 50000; // 100ms
     uint64_t time_us = 0;
     uint64_t time_last_us = 0;
+
+    // Velocity control variables
+    uint32_t update_interval = 1000; // 1ms
+    uint32_t last_update_time = time_us_32();
 
     while (true)
     {
         time_us = time_us_64();
-
         if (abs(velocity) > 0.01)
         {
             float interval = (1000000.0f / 400.f) / (abs(velocity));
@@ -147,8 +195,16 @@ void core1_main()
             }
         }
 
-        // Limit the step holding time to max_step_time_us
-        if (time_us - time_last_us > max_step_time_us)
+        // Update velocity every 1ms
+        time_us = time_us_64();
+        if (time_us - last_update_time >= update_interval)
+        {
+            velocity += acceleration * (time_us - last_update_time) / 1000000.0f;
+            last_update_time += update_interval;
+        }
+
+        // Limit the step holding time to max_interval
+        if (time_us - time_last_us > max_interval)
         {
             for (int i = 0; i < 4; i++)
             {
